d1e29a173a7f5969bb81287a0e660d9037d69776
Im Yongsik <lvhi7121@gmail.com>
2022-02-25 16:50:44 +0900

    feat: Implement callback functions to print Korean

--- Client/Client/Renderer.cpp
+++ Client/Client/Renderer.cpp
@@ -26,11 +26,15 @@ void Renderer::SetupContext(HWND hWnd)
 	m_pd3d = new Direct3D;
 	m_pd3d->Init(hWnd);
 
+	unsigned char* pixel;
+	int width;
+	int height;
+
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
-	io.Fonts->AddFontFromFileTTF("./assets/fonts/OpenSans/OpenSans-Regular.ttf", 20.0f);
 	(void)io;
+	io.Fonts->AddFontFromFileTTF("./assets/fonts/malgun/malgun.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesKorean());
 
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
@@ -64,26 +68,51 @@ void Renderer::Render()
 	ImGui::NewFrame();
 
 	UpdateRenderPos();
-		
+	
 	ImGui::Begin("Chatting Client", nullptr, m_windowFlags);
-	ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-
-	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-
-	if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+	/*if (ImGui::BeginTable("banner", 3))
 	{
-		counter++;
+		ImGui::TableNextColumn();
+		std::string username = u8"ÇÜ½Ä";
+		std::string status = u8"Á¢¼Ó ²÷±è";
+		static std::string userStr = u8"À¯Àú: " + username;
+		ImGui::Text(userStr.c_str());
+
+		ImGui::TableNextColumn();
+		ImGui::Text(status.c_str());
+
+		ImGui::TableNextColumn();
+		ImGui::Button(u8"¼³Á¤", ImVec2(0.0f, ImGui::GetTextLineHeight() * 2));
+
+		ImGui::EndTable();
+	}*/
+	
+
+	ImGui::Text(u8"Á¦ÀÛ: Im Yongsik");               // Display some text (you can use a format strings too)
+	ImGui::Text(u8"Username : ÇÜ½Ä");
+	//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+	//if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+	//{
+	//	counter++;
+	//}
+	//ImGui::SameLine();
+	//ImGui::Text("counter = %d", counter);
+	if (m_chatString.empty())
+	{
+		m_chatString = u8"[ÇÜ½Ä]¿À´Ã Àú³áÀº ÇØ¾¾´Ù!\n[²¿¹Ì]ÇÜ½Ä¾Æ ¹ã¿¡ ÃÂ¹ÙÄûÁ» ¾ÈÅ¸¸é ¾ÈµÇ°Ú´Ï?\n³Ê ¶§¹®¿¡ ÀáÀ» ¸øÀÚ°Ú¾î!\n[ÇÜ½Ä]³ª ´ã´Þ 15ÀÏ¿¡ ¹ÙÇÁ Âï¾î¾ß µÅ.\n±× ¶§±îÁö¸¸ Âü¾Æ!";
 	}
-	ImGui::SameLine();
-	ImGui::Text("counter = %d", counter);
-	char temp[] = "Lorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\nLorem Ipsum\n";
-	ImGui::InputTextMultiline("Chat", temp, ARRAYSIZE(temp), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 20), ImGuiInputTextFlags_ReadOnly | ImGuiWindowFlags_AlwaysVerticalScrollbar);
-	static char inputBuf[64]="";
+	ResizeCallback::MyInputTextMultiline("Chat", &m_chatString, ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 24), ImGuiInputTextFlags_ReadOnly | ImGuiWindowFlags_AlwaysVerticalScrollbar);
 	if (ImGui::BeginTable("Input Table", 2))
 	{
 		ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, 300.0f);
 		ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, 100.0f);
-		ImGui::TableNextColumn(); ImGui::InputTextMultiline("Input", (char*)inputBuf, ARRAYSIZE(inputBuf), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 4.8));
+		if (m_inputString.empty())
+		{
+			m_inputString.push_back('\0');
+		}
+		
+		ImGui::TableNextColumn(); ResizeCallback::MyInputTextMultiline("Input", &m_inputString, ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 4.8));
 		ImGui::TableNextColumn(); 
 		if (ImGui::Button("Enter", ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 4.8)))
 		{
@@ -91,8 +120,6 @@ void Renderer::Render()
 		}
 		ImGui::EndTable();
 	}
-
-
 	ImGui::End();
 
 	ImGui::Render();
@@ -112,5 +139,3 @@ void Renderer::Shutdown()
 	ImGui::DestroyContext();
 	m_pd3d->Shutdown();
 }
-
-
--- Client/Client/Renderer.h
+++ Client/Client/Renderer.h
@@ -5,12 +5,39 @@
 
 #include <string>
 
+// Wrapping ImGui InputText function to print Korean.
+struct ResizeCallback
+{
+	static int InputCallback(ImGuiInputTextCallbackData* data)
+	{
+		if (data->EventFlag == ImGuiInputTextFlags_CallbackResize)
+		{
+			std::string* str = (std::string*)data->UserData;
+			if (str->empty())
+			{
+				str->push_back('\0');
+			}
+			str->resize(data->BufSize);
+			data->Buf = (char*)str->c_str();
+		}
+		return 0;
+	}
+
+	static bool MyInputTextMultiline(const char* label, std::string* str, const ImVec2& size = ImVec2(0, 0), ImGuiInputTextFlags flags = 0)
+	{
+		IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+		return ImGui::InputTextMultiline(label, (char*)str->c_str(), (size_t)str->size(), size, flags | ImGuiInputTextFlags_CallbackResize, ResizeCallback::InputCallback, (void*)str);
+	}
+};
+
 class Renderer
 {
 private:
 	HWND m_hWnd;
 	Direct3D* m_pd3d;
 	ImGuiWindowFlags m_windowFlags;
+	std::string m_chatString;
+	std::string m_inputString;
 
 public:
 
