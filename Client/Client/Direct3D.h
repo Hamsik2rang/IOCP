23796eeca7d12bfe259d40721dfe96ce713ceb4c
Im Yongsik <lvhi7121@gmail.com>
2022-02-25 15:16:12 +0900

    docs: Change directory & Add a font

---
+++ Client/Client/Direct3D.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <d3d11.h>
+
+#include "./ImGui/imgui.h"
+#include "./ImGui/imgui_impl_dx11.h"
+
+
+class Direct3D
+{
+private:
+	HWND m_hWnd;
+
+	ID3D11Device* m_pd3dDevice = nullptr;
+	ID3D11DeviceContext* m_pd3dDeviceContext = nullptr;
+	IDXGISwapChain* m_pSwapChain = nullptr;
+	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;
+
+	void CleanupDevice();
+	void CreateRenderTarget();
+	void CleanupRenderTarget();
+	bool CreateDevice();
+public:
+	~Direct3D();
+	bool Init(HWND hWnd);
+	ID3D11Device* GetDevice();
+	ID3D11DeviceContext* GetDeviceContext();
+
+	void Render();
+	void Shutdown();
+};
+
